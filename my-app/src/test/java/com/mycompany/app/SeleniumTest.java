bf053e3d9965a111b1476103acdd9ca7ba58cc17
aishwaryamalviya <74872007+aishwaryamalviya@users.noreply.github.com>
2022-01-10 16:14:52 +0530

    changed

--- my-app/src/test/java/com/mycompany/app/SeleniumTest.java
+++ my-app/src/test/java/com/mycompany/app/SeleniumTest.java
@@ -7,7 +7,7 @@ public class SeleniumTest {
 
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-    	System.setProperty("webdriver.gecko.driver","C:\\Users\AishwaryaM2\Downloads\geckodriver.exe");
+    	System.setProperty("webdriver.gecko.driver","C://Users/AishwaryaM2/Downloads/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		//comment the above 2 lines and uncomment below 2 lines to use Chrome
 		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
